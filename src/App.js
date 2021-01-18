dc3dc1bcc8346ffeb793ce2e384a7f7b3c23a94f
Valentyn S. Uskov <just.valentine.now@gmail.com>
2021-01-18 10:47:32 +0300

    Remove rough files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3005/graphql'
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client="client">
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <section>MAIN SECTION</section>
+      </div>
+    </ApolloProvider>
   );
 }
 
